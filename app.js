165699acc37258485f110f9a135a11a7a6ccb465
gph <gph1990@live.com>
2022-08-24 17:18:34 -0300

    getting data from api and generating xml

--- app.js
+++ app.js
@@ -34,23 +34,22 @@ ws.cell(1, 1, 1, 4, true)
     .string("Countries List")
     .style(titleStyle);
 
-ws.cell(2,1)
+ws.cell(2, 1)
     .string("Name")
     .style(headerStyle);
-ws.cell(2,2)
+ws.cell(2, 2)
     .string("Capital")
     .style(headerStyle);
-ws.cell(2,3)
+ws.cell(2, 3)
     .string("Area")
     .style(headerStyle);
-ws.cell(2,4)
+ws.cell(2, 4)
     .string("Currencies")
     .style(headerStyle);
 
-let rowCount = 3;
-
 const countriesList = [];
 
+console.log("Getting data from..." + URL);
 axios.get(URL)
     .then(response => {
         const countries = response.data;
@@ -59,42 +58,47 @@ axios.get(URL)
                 name: country.name.common,
                 capital: country.capital ? country.capital : "-",
                 area: country.area ? country.area : "-",
-                curriencies: country.currencies ? Object.keys(country.currencies).toString() : "-"
+                currencies: country.currencies ? Object.keys(country.currencies).toString() : "-"
             });
-
-            // // Name
-            // ws.cell(rowCount,1).string(country.name.common);
-
-            // // Capital
-            // if (country.capital) {
-            //     ws.cell(rowCount,2).string(country.capital[0]);
-            // }else{
-            //     ws.cell(rowCount,2).string("-");
-            // }
-
-            // // Area
-            // if(country.area){
-            //     ws.cell(rowCount,3)
-            //         .number(country.area)
-            //         .style(numberStyle);
-            // }else{
-            //     ws.cell(rowCount,3).string("-");
-            // }
-
-            // // Currency
-            // if (country.currencies) {
-            //     ws.cell(rowCount,4).string(Object.keys(country.currencies).toString());
-            // } else {
-            //     ws.cell(rowCount,4).string("-");
-            // }
-            // rowCount++;
         });
-        countriesList.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))
+        countriesList.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))
 
-        countriesList.forEach(country=>{
-            console.log(country.name);
+        console.log("Creating the spreadsheet...")
+        // Data to Excel Sheet
+        let rowCount = 3;
+        countriesList.forEach(country => {
+            // Name
+            ws.cell(rowCount, 1).string(country.name);
+            // Capital
+            if (country.capital) {
+                ws.cell(rowCount, 2).string(country.capital);
+            } else {
+                ws.cell(rowCount, 2).string("-");
+            }
+            // Area
+            if (country.area) {
+                ws.cell(rowCount, 3)
+                    .number(country.area)
+                    .style(numberStyle);
+            } else {
+                ws.cell(rowCount, 3).string("-");
+            }
+
+            // Currency
+            if (country.currencies) {
+                ws.cell(rowCount, 4).string(country.currencies);
+            } else {
+                ws.cell(rowCount, 4).string("-");
+            }
+            rowCount++;
         });
-        wb.write('countries-list.xlsx');
+        wb.write('countries-list.xlsx', function (err, stats) {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log(__dirname + "/countries-list.xlsx was created successfully!");
+            }
+        });
     })
     .catch(error => {
         console.log(error);
